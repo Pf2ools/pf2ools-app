da2ffc151b2e4e2cfd7644e96b6bcac404d9a8a4
MrVauxs <mrvauxs@gmail.com>
2023-12-01 16:06:01 +0100

    init

---
+++ svelte.config.js
@@ -0,0 +1,20 @@
+import adapter from '@sveltejs/adapter-static'
+import preprocess from 'svelte-preprocess';
+
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	extensions: ['.svelte'],
+	preprocess: preprocess({
+		sourceMap: true,
+		aliases: []
+	}),
+
+	vitePlugin: {
+		inspector: true,
+	},
+	kit: {
+		adapter: adapter()
+	}
+};
+export default config;
