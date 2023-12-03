81ffd5c70d1a525c2d16d761696ebf7d8d2f35d4
MrVauxs <mrvauxs@gmail.com>
2023-12-03 02:41:47 +0100

    fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,33 @@ export default {
 		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}'),
 	],
 	theme: {
-		extend: {},
+		extend: {
+			spacing: {
+				'1/10': '10%',
+				'2/10': '20%',
+				'3/10': '30%',
+				'4/10': '40%',
+				'5/10': '50%',
+				'6/10': '60%',
+				'7/10': '70%',
+				'8/10': '80%',
+				'9/10': '90%',
+			},
+			fontFamily: {
+				flavor: ['var(--theme-font-family-flavor)'],
+				action: ['pathfinder2eactions'],
+			},
+		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+				sm: '2rem',
+				lg: '4rem',
+				xl: '6rem',
+				'2xl': '4rem',
+			},
+		},
 	},
 	plugins: [
 		forms,
