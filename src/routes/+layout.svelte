165db6f2b91e52a69781aedbdd758a25330910c0
MrVauxs <mrvauxs@gmail.com>
2023-12-03 18:37:25 +0100

    very cool header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,22 +17,37 @@
 
 	// Floating UI for Popups
 	import { arrow, autoUpdate, computePosition, flip, offset, shift } from '@floating-ui/dom';
-	import { AppBar, storePopup, AppShell, TabGroup, TabAnchor } from '@skeletonlabs/skeleton';
+	import { AppBar, AppShell, TabAnchor, TabGroup, storePopup } from '@skeletonlabs/skeleton';
+	import { slide } from 'svelte/transition';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
-	import { page } from '$app/stores';
+	let headline = false;
+
+	let timer: NodeJS.Timeout | undefined;
+	const debounce = (value: boolean) => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			headline = value;
+		}, 100);
+	};
 </script>
+
+<svelte:head>
+	<title>pf2ools</title>
+</svelte:head>
 
 <AppShell>
 	<svelte:fragment slot="header">
-		<AppBar shadow="shadow-2xl" gap="gap-8" padding="px-8">
+		<AppBar shadow="shadow-2xl" gap="gap-8" padding="px-8" spacing="">
 			<svelte:fragment slot="lead">
-				<img src="icons/source/pf2e.svg" alt="pf2ools logo" class="h-8 scale-200" />
+				<div class="absolute py-5 hidden sm:flex">
+					<img src="icons/source/pf2e.svg" alt="pf2ools logo" class="w-8 scale-200" />
+					<div class="pl-8 pt-1">pf2ools</div>
+				</div>
 			</svelte:fragment>
 
 			<div class="flex">
-				<div class="absolute py-5 hidden sm:block">pf2ools</div>
-				<div class="mx-auto">
+				<div class="mx-auto" aria-level="0">
 					<TabGroup
 						justify="justify-center"
 						active="variant-filled-primary"
@@ -43,46 +58,64 @@
 						class=""
 					>
 						<TabAnchor
-							class="w-16 h-16 text-xs"
+							class="w-24 h-16 text-xs"
 							padding="py-2"
-							href="/players"
-							selected={$page.url.pathname === '/players'}
+							on:mouseover={() => debounce(true)}
+							on:mouseleave={() => debounce(false)}
 						>
 							<svelte:fragment slot="lead">(ico)</svelte:fragment>
-							<span>(players)</span>
+							<span>(rules)</span>
 						</TabAnchor>
 						<TabAnchor
-							class="w-16 h-16 text-xs"
+							class="w-24 h-16 text-xs"
 							padding="py-2"
-							href="/gm"
-							selected={$page.url.pathname === '/gm'}
+							on:mouseover={() => debounce(true)}
+							on:mouseleave={() => debounce(false)}
 						>
 							<svelte:fragment slot="lead">(ico)</svelte:fragment>
-							<span>(gm)</span>
+							<span>(player)</span>
 						</TabAnchor>
 						<TabAnchor
-							class="w-16 h-16 text-xs"
+							class="w-24 h-16 text-xs"
 							padding="py-2"
-							href="/"
-							selected={$page.url.pathname === '/'}
+							on:mouseover={() => debounce(true)}
+							on:mouseleave={() => debounce(false)}
+						>
+							<svelte:fragment slot="lead">(ico)</svelte:fragment>
+							<span>(game master)</span>
+						</TabAnchor>
+						<TabAnchor
+							class="w-24 h-16 text-xs"
+							padding="py-2"
+							on:mouseover={() => debounce(true)}
+							on:mouseleave={() => debounce(false)}
 						>
 							<svelte:fragment slot="lead">(ico)</svelte:fragment>
 							<span>(search)</span>
 						</TabAnchor>
 						<TabAnchor
-							class="w-16 h-16 text-xs"
+							class="w-24 h-16 text-xs"
 							padding="py-2"
-							href="/references"
-							selected={$page.url.pathname === '/references'}
+							on:mouseover={() => debounce(true)}
+							on:mouseleave={() => debounce(false)}
 						>
 							<svelte:fragment slot="lead">(ico)</svelte:fragment>
 							<span>(references)</span>
 						</TabAnchor>
 						<TabAnchor
-							class="w-16 h-16 text-xs"
+							class="w-24 h-16 text-xs"
 							padding="py-2"
-							href="/settings"
-							selected={$page.url.pathname === '/settings'}
+							on:mouseover={() => debounce(true)}
+							on:mouseleave={() => debounce(false)}
+						>
+							<svelte:fragment slot="lead">(ico)</svelte:fragment>
+							<span>(utilities)</span>
+						</TabAnchor>
+						<TabAnchor
+							class="w-24 h-16 text-xs"
+							padding="py-2"
+							on:mouseover={() => debounce(true)}
+							on:mouseleave={() => debounce(false)}
 						>
 							<svelte:fragment slot="lead">(ico)</svelte:fragment>
 							<span>(settings)</span>
@@ -90,7 +123,24 @@
 					</TabGroup>
 				</div>
 			</div>
-			<!-- <svelte:fragment slot="headline">(headline)</svelte:fragment> -->
+			<svelte:fragment slot="headline">
+				{#if headline}
+					<div
+						class="flex absolute w-full bg-surface-100-800-token -mx-8"
+						in:slide={{ duration: 300 }}
+						out:slide={{ duration: 300, delay: 500 }}
+						role="heading"
+						aria-level="1"
+						aria-label="Headline"
+						on:mouseover={() => debounce(true)}
+						on:mouseleave={() => debounce(false)}
+						on:focus={() => debounce(true)}
+						on:blur={() => debounce(false)}
+					>
+						<div class="mx-auto">(headline)</div>
+					</div>
+				{/if}
+			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
 	<!-- (sidebarLeft) -->
