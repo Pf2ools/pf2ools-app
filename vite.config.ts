c4083a5ae2eca1269c9fd625c72d0b984e6814ea
MrVauxs <mrvauxs@gmail.com>
2023-12-15 15:25:16 +0100

    reserve contentManager

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	build: {
+		terserOptions: {
+			mangle: {
+				reserved: ['contentManager'],
+			},
+		},
+	},
 	plugins: [sveltekit(), purgeCss()],
 });
