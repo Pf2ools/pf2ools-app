1787ade05f1518259d77c0b0c45fcc7edeb01f76
MrVauxs <mrvauxs@gmail.com>
2023-12-15 13:24:21 +0100

    rudimentary background browsing

--- src/routes/backgrounds/+page.svelte
+++ src/routes/backgrounds/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import cm from '$lib/data/contentManager';
 	const { backgrounds } = cm;
+
+	let selected = $backgrounds[0];
 </script>
 
 <svelte:head>
@@ -8,9 +10,24 @@
 </svelte:head>
 
 <div class="container flex justify-center h-full">
-	<div class="text-center border w-full h-slot overflow-scroll">
-		{#each $backgrounds as bg}
-			<p>{bg.name.primary} from {bg.source.ID}</p>
-		{/each}
+	<div class="text-center border w-full h-slot grid grid-cols-2">
+		<div class="overflow-scroll flex flex-col">
+			{#each $backgrounds as bg}
+				<button on:click={() => (selected = bg)}>{bg.name.primary} from {bg.source.ID}</button>
+			{/each}
+		</div>
+		<div class="overflow-scroll flex flex-col">
+			<div class="pt-2">
+				{#if selected}
+					<h1 class="h2">{selected.name.primary}</h1>
+					<p>Source: {selected.source.ID}</p>
+					<div>
+						{#each selected.data.entries as entry}
+							<p>{entry}</p>
+						{/each}
+					</div>
+				{/if}
+			</div>
+		</div>
 	</div>
 </div>
