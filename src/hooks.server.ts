a74284212a4862f7e169a08ea3138b57924c2bc9
MrVauxs <mrvauxs@gmail.com>
2024-01-20 00:16:46 +0100

    Fix the og:title on homepage

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,10 +7,12 @@ export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event, {
 		// TODO: Replace the title with the actual page title rather than basing it off the URL.
 		transformPageChunk: ({ html }) =>
-			html.replace(
-				'<meta property="og:title" content="pf2ools" />',
-				`<meta property="og:title" content="${toTitleCase(event.url.pathname.replace('/', ''))} - pf2ools" />`
-			),
+			event.url.pathname === '/'
+				? html
+				: html.replace(
+						'<meta property="og:title" content="pf2ools" />',
+						`<meta property="og:title" content="${toTitleCase(event.url.pathname.replace('/', ''))} - pf2ools" />`
+					),
 	});
 
 	/*
