6ecc6294257b66abcc950cc379d0a36ff5607a7d
MrVauxs <mrvauxs@gmail.com>
2024-01-20 04:40:54 +0100

    lets try this

--- src/routes/backgrounds/[slug]/+server.ts
+++ src/routes/backgrounds/[slug]/+server.ts
@@ -1,14 +1,35 @@
 import { type RequestHandler } from '@sveltejs/kit';
 import type { EntryGenerator } from './$types';
 import { CF_PAGES, SEO } from '$env/static/private';
+import { base } from '$app/paths';
 
 export const GET: RequestHandler = ({ params: { slug } }) => {
 	console.log(slug);
 
-	// TODO: replace with +page.svelte with wanted metadata and a JS redirect
-
+	// TODO: respond with a HTML page of wanted metadata and a JS redirect to the previous page with hash
 	return new Response(
-		`Go no further, wretched intruder. You have attempted entry into the hitherto unbuilt sanctum, named by decree "${slug}". Your fortune has thus turned. Exeunt, and reconsider your actions in the morrow.`
+		`<!DOCTYPE html>
+		<html lang="en">
+			<head>
+				<meta charset="utf-8" />
+				<title>${slug} - pf2ools</title>
+				<meta name="description"  content="${slug}" />
+				<meta property="og:title" content="${slug} - pf2ools" />
+				<meta property="og:description" content="${slug}" />
+			</head>
+			<body style="background: black; color: gray">
+				You are getting redirected to "${base}/backgrounds#${slug}".
+			</body>
+			<script>
+				window.location.replace("${base}/backgrounds#" + "${slug}");
+			</script>
+		</html>`,
+		{
+			headers: {
+				'content-type': 'text/html; charset=utf-8',
+				'cache-control': 's-maxage=1, stale-while-revalidate',
+			},
+		}
 	);
 };
 
