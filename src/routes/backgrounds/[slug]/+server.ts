309d70ef06cb19c63d27311e5125f723a855eb06
MrVauxs <mrvauxs@gmail.com>
2024-01-20 04:28:22 +0100

    Revert "an attempt is being made"
    This reverts commit b09069d9799bde7002c7491d6739c7d1909f9f0b.

---
+++ src/routes/backgrounds/[slug]/+server.ts
@@ -0,0 +1,23 @@
+import { type RequestHandler } from '@sveltejs/kit';
+import type { EntryGenerator } from './$types';
+import { CF_PAGES, SEO } from '$env/static/private';
+
+export const GET: RequestHandler = ({ params: { slug } }) => {
+	console.log(slug);
+
+	// TODO: replace with +page.svelte with wanted metadata and a JS redirect
+
+	return new Response(
+		`Go no further, wretched intruder. You have attempted entry into the hitherto unbuilt sanctum, named by decree "${slug}". Your fortune has thus turned. Exeunt, and reconsider your actions in the morrow.`
+	);
+};
+
+export const entries: EntryGenerator = async () => {
+	return [
+		{
+			slug: "Abadar's Avenger_FRP0",
+		},
+	];
+};
+
+export const prerender = CF_PAGES || SEO ? 'auto' : false;
