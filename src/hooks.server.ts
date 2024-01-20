c60f1b9f0858fb67f2c36ace8a990113f3f949fd
MrVauxs <mrvauxs@gmail.com>
2024-01-20 01:01:38 +0100

    well thats a shame

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,16 @@
-import type { Handle } from '@sveltejs/kit';
 import { toTitleCase } from '$lib/utils';
+import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = ({ event, resolve }) => {
-	console.log('Hook:', event.url.pathname);
+	const url = event.url.pathname;
 
-	if (event.url.pathname !== '/') {
+	if (url !== '/') {
 		const response = resolve(event, {
 			// TODO: Replace the title with the actual page title rather than basing it off the URL.
 			transformPageChunk: ({ html }) =>
 				html.replace(
 					'<meta property="og:title" content="pf2ools" />',
-					`<meta property="og:title" content="${toTitleCase(event.url.pathname.replace('/', ''))} - pf2ools" />`
+					`<meta property="og:title" content="${toTitleCase(url.replace('/', ''))} - pf2ools" />`
 				),
 		});
 
@@ -19,9 +19,3 @@ export const handle: Handle = ({ event, resolve }) => {
 
 	return resolve(event);
 };
-
-/*
-	TODO: use the 'reroute' hook to redirect shareable pages to a hash-based URL.
-	Example: backgrounds/Academy%20Dropout_SoM => backgrounds#Academy%20Dropout_SoM
-	Additionally, add embeds.
-*/
