b09069d9799bde7002c7491d6739c7d1909f9f0b
MrVauxs <mrvauxs@gmail.com>
2024-01-20 04:17:53 +0100

    an attempt is being made

---
+++ src/routes/backgrounds/[slug]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { goto } from '$app/navigation';
+
+	export let data: PageData;
+
+	goto(data.goto);
+</script>
+
+<svelte:head>
+	<meta name="description" content={data.hash} />
+	<meta name="og:description" content={data.hash} />
+	<meta property="og:title" content={data.hash} />
+</svelte:head>
