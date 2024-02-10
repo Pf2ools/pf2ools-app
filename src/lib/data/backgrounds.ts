921879c808d4f2b73183c3af391722ec2414ba3a
MrVauxs <mrvauxs@gmail.com>
2024-02-11 00:03:17 +0100

    gah

--- src/lib/data/backgrounds.ts
+++ src/lib/data/backgrounds.ts
@@ -1,6 +1,7 @@
 import backgroundClass from './classes/backgroundClass';
 import { background as backgroundData } from './pf2ools-data/bundles/byDatatype/core/background.json' assert { type: 'json' };
 import { background as backgroundSchema } from 'pf2ools-schema';
+// ==========
 import { derived, get } from 'svelte/store';
 import { homebrew } from './homebrew';
 import type { z } from 'zod';
@@ -9,10 +10,18 @@ export const backgrounds = {
 	data: backgroundData as z.infer<typeof backgroundSchema>[],
 	schema: backgroundSchema,
 	class: backgroundClass,
+	homebrew: {
+		store: derived(homebrew.store, ($hb) => $hb.map((b) => b.background ?? []).flat()),
+		get contents() {
+			return get(this.store);
+		},
+	},
 	get contents() {
 		return get(this.store);
 	},
 	get store() {
-		return derived(homebrew, ($hb) => [...this.data, ...$hb].map((b) => new this.class(b)));
+		return derived(this.homebrew.store, ($hb) =>
+			[...this.data, ...$hb].map((b) => new this.class(b))
+		);
 	},
 };
--- src/lib/data/homebrew.ts
+++ src/lib/data/homebrew.ts
@@ -1,3 +1,20 @@
 import { localStorageStore } from '@skeletonlabs/skeleton';
+import { get, type Writable } from 'svelte/store';
+import type { z } from 'zod';
+import { bySource } from 'pf2ools-schema';
 
-export const homebrew = localStorageStore('homebrew', []);
+interface Homebrew {
+	store: Writable<z.infer<typeof bySource>[]>;
+	contents: z.infer<typeof bySource>[];
+	removeID: (id: string) => void;
+}
+
+export const homebrew: Homebrew = {
+	store: localStorageStore('homebrew', []),
+	get contents() {
+		return get(this.store);
+	},
+	removeID(id: string) {
+		this.store.update((hb) => hb.filter((b) => b.source.ID !== id));
+	},
+};
